6faf90883da58f734a8a48f985690162d4b9734d
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-06-01 23:54:29 +0500

    bug fixed for order summery section shopping cart

--- components/shopping-cart/TotalCheckOutCard.tsx
+++ components/shopping-cart/TotalCheckOutCard.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import PageTitle, { Textsm } from "../PageTitle";
 
 const TotalCheckOutCard = () => {
+  let subtotal = 1190;
+  let discount = 140;
+  let shippingcharges = 500;
+  let total = subtotal - discount + shippingcharges;
   return (
     <div
       className="px-2 md:px-4 py-2 md:py-4 bg-white rounded-xl
@@ -11,23 +15,23 @@ const TotalCheckOutCard = () => {
         Order Summary
       </Textsm>
       <hr className="my-5" />
-      <div className="flex flex-col">
+      <div className="flex flex-col font- font-semibold text-[15px]">
         <div className="flex items-center justify-between">
           <p>Subtotal</p>
-          <p>Rs/- 190.00</p>
+          <p>Rs/- {subtotal.toFixed(2)}</p>
         </div>
         <div className="flex items-center justify-between">
           <p>Discount</p>
-          <p>Rs/- 40.00</p>
+          <p className="text-red-500">Rs/- {discount.toFixed(2)}</p>
         </div>
         <div className="flex items-center justify-between">
           <p>Shipping</p>
-          <p>Rs/- 120.00</p>
+          <p>Rs/- {shippingcharges.toFixed(2)}</p>
         </div>
       </div>
       <hr className="my-5" />
       <Textsm className="text-gray-700 text-[20px] font-bold">
-        Total Rs/- 430.00
+        Total Rs/- {total.toFixed(2)}
       </Textsm>
 
       <label className="flex items-center space-x-2 text-sm gap-3">
@@ -40,7 +44,9 @@ const TotalCheckOutCard = () => {
         I agreed with the terms and conditions
       </label>
       <div className="flex justify-center mt-3">
-      <button className="px-3 py-1.5 bg-black text-white hoverEffect hover:bg-primary-color rounded">Proceed to checkout</button>
+        <button className="px-3 py-1.5 bg-black text-white hoverEffect hover:bg-primary-color rounded">
+          Proceed to checkout
+        </button>
       </div>
     </div>
   );
